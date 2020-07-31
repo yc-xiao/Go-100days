7caa5a2a7a96cc74f6ccb2c273a718c08cf37393
youcan <youcan.xiao@sagene.com.cn>
2020-07-31 10:11:37 +0800

    mod

---
+++ go.mod
@@ -0,0 +1,8 @@
+module Go-100days/learn
+
+go 1.14
+
+require (
+	github.com/golang/mock v1.4.4 // indirect
+	golang.org/x/tools v0.0.0-20200730221956-1ac65761fe2c // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	_ "Go-100days/codes/mods/m1"
+    "Go-100days/learn/Day1-20/Day01-15/code/day06"
+    "fmt"
 )
 
 func main() {
-
+    day06.Pmain6()
+    fmt.Println(2333)
 }
 
