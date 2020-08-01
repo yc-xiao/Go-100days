6b7f8e65e2d6a60610f6b2d38306dc4ccc6a9159
yc <yc@qq.com>
2020-08-01 16:05:13 +0800

    mmap

--- codes/pkgs/mmaps/main.go
+++ codes/pkgs/mmaps/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/exp/mmap"
 	"io"
 	"log"
 	"os"
@@ -21,10 +22,9 @@ func testRW(oldPath, newPath string)  {
 
 	defer fReader.Close()
 	defer fWriter.Close()
-	//io.Copy(fReader, fWriter)
-	//return
 
-	var block = make([]byte, 1024 * 1024 * 1024)
+	mbUnit := 1024 * 1024 * 1024
+	var block = make([]byte, mbUnit)
 	for {
 		n, err := fReader.Read(block)
 		if err != nil && err != io.EOF {
@@ -41,17 +41,70 @@ func testRW(oldPath, newPath string)  {
 
 }
 
+func testIoPkg(oldPath, newPath string) {
+	lable := "testIoPkg"
+	fReader, err := os.Open(oldPath)
+	if err != nil {
+		log.Fatal(lable, " reader err ", err)
+	}
+	fWriter, err := os.OpenFile(newPath, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		log.Fatal(lable, " writer err ", err)
+	}
+
+	defer fReader.Close()
+	defer fWriter.Close()
+	io.Copy(fWriter, fReader)
+}
+
 func testMMAPRW(oldPath, newPath string)  {
-	//lable := "testMMAPRW"
+	lable := "testMMAPRW"
+
+	fReader, err := mmap.Open(oldPath)
+	if err != nil {
+		log.Fatal(lable, " reader err ", err)
+	}
+
+	fWriter, err := os.OpenFile(newPath, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		log.Fatal(lable, " writer err ", err)
+	}
+
+	defer fReader.Close()
+	defer fWriter.Close()
+
+	mbUnit := 1024 * 1024 * 1024
+	var block = make([]byte, mbUnit)
+	var n int
+	for {
+		n, err = fReader.ReadAt(block, int64(n))
+		if err != nil && err != io.EOF {
+			log.Fatal(lable, " read err ", err)
+		}
+		if err == io.EOF || n == 0 {
+			break
+		}
+		_, err = fWriter.Write(block[:n])
+		if err != nil {
+			log.Fatal(lable, " write err ", err)
+		}
+	}
+
 }
 
 func main() {
-	old, new := "/home/youcan/Desktop/tt.tar", "/home/youcan/Desktop/t2.tar"
-	s := time.Now()
+	old, new := "/home/youcan/Desktop/1.tar.gz", "/home/youcan/Desktop/2.tar.gz"
+	s0 := time.Now()
 	testRW(old, new)
 	s1 := time.Now()
-	//testMMAPRW(old, new)
+	testIoPkg(old, new)
 	s2 := time.Now()
-	fmt.Println("mmap -> ", s2.Sub(s1), 's')
-	fmt.Println("norm -> ", s1.Sub(s), 's')
+	testMMAPRW(old, new)
+	s3 := time.Now()
+
+	fmt.Println("norm -> ", s1.Sub(s0))
+	fmt.Println("iocp -> ", s2.Sub(s1))
+	fmt.Println("mmap -> ", s3.Sub(s2))
+
+
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/golang/mock v1.4.4 // indirect
+	golang.org/x/exp v0.0.0-20200513190911-00229845015e
 	golang.org/x/tools v0.0.0-20200730221956-1ac65761fe2c // indirect
 )
