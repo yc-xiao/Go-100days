6373724111f9a8a11ad5401ef873f37adc27be2d
youcan <youcan.xiao@sagene.com.cn>
2020-08-03 08:58:45 +0800

    context

--- codes/examples/console_gorountine/context.go
+++ codes/examples/console_gorountine/context.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
+func reqTask(ctx context.Context, name string) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("stop", name)
+			return
+		}
+	}
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	go reqTask(ctx, "worker1")
+	time.Sleep(time.Second)
+	cancel()
+	time.Sleep(time.Second)
 }
